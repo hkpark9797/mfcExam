417fdda390dd52da95a3f05c9e8341b89125da28
hkpark9797 <ite97@naver.com>
2024-08-09 22:36:35 +0900

    new dialog

---
+++ prj/prjDlg.h
@@ -0,0 +1,37 @@
+﻿
+// prjDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CprjDlg 대화 상자
+class CprjDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CprjDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PRJ_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedBtnDlg();
+};
