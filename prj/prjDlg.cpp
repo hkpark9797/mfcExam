79082ac073941d10f5805f1da77b32424ad19ec2
hkpark9797 <ite97@naver.com>
2024-08-10 15:31:22 +0900

    우측 이미지에 정보 전달

--- prj/prjDlg.cpp
+++ prj/prjDlg.cpp
@@ -7,6 +7,9 @@
 #include "prj.h"
 #include "prjDlg.h"
 #include "afxdialogex.h"
+#include <iostream>
+
+using namespace std;
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -66,8 +69,8 @@ BEGIN_MESSAGE_MAP(CprjDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
-	ON_BN_CLICKED(IDC_BTN_DLG, &CprjDlg::OnBnClickedBtnDlg)
 	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_TEST, &CprjDlg::OnBnClickedBtnTest)
 END_MESSAGE_MAP()
 
 
@@ -103,9 +106,17 @@ BOOL CprjDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	MoveWindow(0, 0, 1280, 800);
 	m_pDlgImage = new CDlgImage;
 	m_pDlgImage->Create(IDD_CDlgImage, this);
 	m_pDlgImage->ShowWindow(SW_SHOW);
+	//m_pDlgImage->MoveWindow(0, 0, 640, 480);
+
+	m_pDlgImgResult = new CDlgImage;
+	m_pDlgImgResult->Create(IDD_CDlgImage, this);
+	m_pDlgImgResult->ShowWindow(SW_SHOW);
+	m_pDlgImgResult->MoveWindow(640, 0, 640, 480);
+
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
 
@@ -159,27 +170,49 @@ HCURSOR CprjDlg::OnQueryDragIcon()
 }
 
 
-
-void CprjDlg::OnBnClickedButton1()
-{
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-}
-
-
-void CprjDlg::OnBnClickedBtnDlg()
-{
-	m_pDlgImage->ShowWindow(SW_SHOW);
-}
-
-
 void CprjDlg::OnDestroy()
 {
 	CDialogEx::OnDestroy();
-
-	delete m_pDlgImage;
+	if(m_pDlgImage)		delete m_pDlgImage;
+	if(m_pDlgImgResult) delete m_pDlgImgResult;
 }
-#include <iostream>
+
 void CprjDlg::callFunc(int n) {
 	//int nData = n;
 	std::cout << n << std::endl;
+}
+
+
+void CprjDlg::OnBnClickedBtnTest()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	unsigned char* fm = (unsigned char*)m_pDlgImage->m_image.GetBits();
+	int nWidth = m_pDlgImage->m_image.GetWidth();
+	int nHeight = m_pDlgImage->m_image.GetHeight();
+	int nPitch = m_pDlgImage->m_image.GetPitch();
+
+	memset(fm, 0xff, nWidth * nHeight);
+
+	for (int k = 0; k < 100; k++) {
+		int x = rand() % nWidth;
+		int y = rand() % nHeight;
+		fm[y * nPitch + x] = 0;
+	}
+
+	int nIndex = 0;
+
+	for (int j = 0; j < nHeight; j++) {
+		for (int i = 0; i < nWidth; i++) {
+			if (fm[j * nPitch + i] == 0) {
+				if (m_pDlgImgResult->m_nDataCount < 100) {
+					m_pDlgImgResult->m_ptData[nIndex].x = i;
+					m_pDlgImgResult->m_ptData[nIndex].y = j;
+					m_pDlgImgResult->m_nDataCount = ++nIndex;
+				}
+			}
+		}
+	}
+
+	m_pDlgImage->Invalidate();
+	m_pDlgImgResult->Invalidate();
 }
